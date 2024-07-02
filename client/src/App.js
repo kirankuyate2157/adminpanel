003f47f1624279af4901460dddd10b914a152b02
Kiran kuyate <84271800+kirankuyate2157@users.noreply.github.com>
2024-07-02 23:28:04 +0530

    server setup

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Toaster } from 'react-hot-toast';
 import Auth from './components/Auth';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/auth" element={<Auth />} />
-        <Route path="/home" element={<div>in App routes</div>} />
+        <Route path="/home" element={<Home/>} />
         <Route path="*" element={<div>in App routes</div>} />
       </Routes>
     </div>
