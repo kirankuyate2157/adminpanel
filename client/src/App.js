7d4597bde78b566f1f100cc8b93275ff7eea52f9
Kiran kuyate <84271800+kirankuyate2157@users.noreply.github.com>
2024-07-03 10:35:26 +0530

    DEV: layout and cure user status and logout added

--- client/src/App.js
+++ client/src/App.js
@@ -5,19 +5,22 @@ import { Toaster } from 'react-hot-toast';
 import Auth from './components/Auth';
 import Home from './components/Home';
 import axios from 'axios';
+import HomeLayout from './layout/HomeLayout';
 
 
 function App() {
   axios.defaults.baseURL = "http://localhost:8080/api/v1";
   axios.defaults.params = {};
   axios.defaults.withCredentials = true;
-  
+
   return (
     <div className="App">
       <Routes>
         <Route path="/auth" element={<Auth />} />
-        <Route path="/home" element={<Home/>} />
-        <Route path="*" element={<div>in App routes</div>} />
+        <Route path="/" element={<HomeLayout />} >
+          <Route path="/home" element={<Home />} />
+          <Route path="*" element={<div>in App routes</div>} />
+        </Route>
       </Routes>
     </div>
   );
--- client/src/components/Auth/utils/authApi.js
+++ client/src/components/Auth/utils/authApi.js
@@ -18,3 +18,21 @@ export const loginUser = async (userData) => {
         throw error.response.data.message || 'Error logging in user';
     }
 };
+
+export const currentUser = async () => {
+    try {
+        const response = await axios.get('/users/get-current-user');
+        return response.data.data;
+    } catch (error) {
+        throw error.response.data.message || 'Error registering user';
+    }
+};
+
+export const logOutUser = async () => {
+    try {
+        const response = await axios.post('/users/logout');
+        return response.data;
+    } catch (error) {
+        throw error.response.data.message || 'Error registering user';
+    }
+};
