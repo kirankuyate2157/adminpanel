dd55da47efe7a9214bc9cd64fc11b9d4546c3d68
Kiran kuyate <84271800+kirankuyate2157@users.noreply.github.com>
2024-07-03 16:02:57 +0530

    CORS fix

--- server/src/app.js
+++ server/src/app.js
@@ -10,12 +10,28 @@ import AdminRoutes from "./routes/admin.routes.js"
 const app = express();
 
 
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: process.env.CORS_ORIGIN,
+//     credentials: true,
+//   })
+// );
+
+app.use(function (req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  const allowedOrigins = process.env.CORS_ORIGIN;
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE, PATCH");
+  next();
+});
+
+
+//routes
 
 app.use(express.json({ limit: "16kb" })); //json upload limit to save server from crash..
 app.use(express.urlencoded({ extended: true, limit: "16kb" })); //url data understanding
